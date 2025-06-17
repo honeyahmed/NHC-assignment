68833d00bdda95335eab79e5b5fe41799a465b3b
Honey Ahmed <52870870+honeyahmed@users.noreply.github.com>
2025-06-17 14:21:01 +0300

    Update not-found.tsx

--- src/app/[id]/not-found.tsx
+++ src/app/[id]/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-100px)]">
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-100px)] px-2">
       <Image src="/not-found.png" alt="Not found" width={200} height={250} />
       <h2 className="text-2xl font-semibold text-nhc-blue mb-4 mt-4">
         Product Not Found
